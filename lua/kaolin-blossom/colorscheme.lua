af0461346aa88daa749b47473989d966bbea0e5c
c6rg0 <gdrozbik08@gmail.com>
2026-07-29 14:19:51 +0100

    Removed template code from forked repo, more trial and error with the colours

--- lua/kaolin-blossom/colorscheme.lua
+++ lua/kaolin-blossom/colorscheme.lua
@@ -5,11 +5,11 @@ local colorscheme = {
   standardBlack = '#161618',
 }
 
-colorscheme.editorBackground = config.transparent and 'none' or '#453038'
-colorscheme.sidebarBackground = '#271f1f'
-colorscheme.popupBackground = '#33242A'
-colorscheme.floatingWindowBackground = '#2E2025'
-colorscheme.menuOptionBackground = '#33242A'
+colorscheme.editorBackground = config.transparent and 'none' or '#2F2025'
+colorscheme.sidebarBackground = '#2F2025'
+colorscheme.popupBackground = '#453038'
+colorscheme.floatingWindowBackground = '#3A2930'
+colorscheme.menuOptionBackground = '#453038'
 
 colorscheme.mainText = '#f2f2f2'
 colorscheme.emphasisText = '#eed891'
@@ -25,7 +25,7 @@ colorscheme.focusedBorder = '#2B2B2F'
 colorscheme.emphasizedBorder = '#303035'
 
 colorscheme.syntaxError = '#e84c58'
-colorscheme.syntaxFunction = '#a0586c'
+colorscheme.syntaxFunction = '#eeeb28'
 colorscheme.warningText = '#f3c91f'
 colorscheme.syntaxKeyword = '#eed891'
 colorscheme.linkText = '#688468'
@@ -34,7 +34,7 @@ colorscheme.warningEmphasis = '#CFB05F'
 colorscheme.successText = '#3e594e'
 colorscheme.errorText = '#e84c58'
 colorscheme.specialKeyword = '#dbb68f'
-colorscheme.commentText = '#6B4B53' -- or #688468
+colorscheme.commentText = '#65a0a1' -- or #688468
 colorscheme.syntaxOperator = '#CD8266'
 colorscheme.foregroundEmphasis = '#18181B'
 colorscheme.terminalGray = '#353b3c'
